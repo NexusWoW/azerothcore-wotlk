0b90a0282af1f68f11e2fbd794ce16ac758d798c
NexusWoW <aussiewowserver@gmail.com>
2017-01-25 14:46:45 +1000

    bug #10 second commit

--- src/scripts/Northrend/Ulduar/HallsOfLightning/boss_bjarngrim.cpp
+++ src/scripts/Northrend/Ulduar/HallsOfLightning/boss_bjarngrim.cpp
@@ -110,20 +110,13 @@ public:
             m_pInstance = creature->GetInstanceScript();
 
             // Init waypoints
-            AddWaypoint(1, 1281.6f, 99.5f, 33.5f, 0);
-            AddWaypoint(2, 1311.7f, 99.4f, 40.1f, 0);
-            AddWaypoint(3, 1332.5f, 99.7f, 40.18f, 0);
-            AddWaypoint(4, 1311.7f, 99.4f, 40.1f, 0);
-            AddWaypoint(5, 1281.6f, 99.5f, 33.5f, 0);
-            AddWaypoint(6, 1262.18f, 99.3f, 33.5f, 0);
-            AddWaypoint(7, 1262, -26.9f, 33.5f, 0);
-            AddWaypoint(8, 1281.2f, -26.8f, 33.5f, 0);
-            AddWaypoint(9, 1311.3f, -26.9f, 40.03f, 0);
-            AddWaypoint(10, 1332, -26.6f, 40.18f, 0);
-            AddWaypoint(11, 1311.3f, -26.9f, 40.03f, 0);
-            AddWaypoint(12, 1281.2f, -26.8f, 33.5f, 0);
-            AddWaypoint(13, 1262, -26.9f, 33.5f, 0);
-	    AddWaypoint(14, 1331, -26.9f, 40.18f, 0);
+            AddWaypoint(1, 1261.6f, 99.5f, 33.5f, 0);
+            AddWaypoint(2, 1262.7f, -26.4f, 33.51f, 0);
+            AddWaypoint(3, 1332.5f, -27.7f, 40.18f, 0);
+            AddWaypoint(4, 1394.9f, 36.4f, 50.03f, 0);
+	    AddWaypoint(5, 1332.5f, -27.7f, 40.18f, 0);
+	    AddWaypoint(6, 1262.7f, -26.4f, 33.51f, 0);
+	    AddWaypoint(7, 1261.6f, 99.5f, 33.5f, 0);
             
             Start(true, false, 0, NULL, false, true);
         }
